5b0639675a8c3150cbb11aaf01be57d628c7194d
Evan Fernando <evansfernando25@gmail.com>
2023-12-27 21:33:20 +0700

    fix to deployment netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls} from "@quasar/vite-plugin"
 
 export default defineConfig({
-  base: '/dashboard',
+  base: '',
   // server: {
   //   proxy: {
   //     '/api': {
